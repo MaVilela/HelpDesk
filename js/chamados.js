0cd07b562590706f2e4dd4db72904b6ed131ddca
Mariana Vilela <mari.vilela1996@gmail.com>
2022-05-08 17:48:04 -0300

    alterando layout do login e alguns arquivos

--- js/chamados.js
+++ js/chamados.js
@@ -2,6 +2,24 @@
 ao fim da seção, logo, podemos fechar e abrir o browser várias vezes e as informações
 gravadas permanecerão lá  */
 // Inicio da Função JSON
+function logar(){
+    var email = document.getElementById("email");
+    var senha = document.getElementById("senha");
+
+    if(email.value == "admin@email.com" && senha.value == "1234"){
+        var autoriza = 1;
+        localStorage.setItem("acesso", true);
+        window.location.href = "editarchamado.html";
+
+    }else if(email.value == "user@email.com" && senha.value == "1234"){
+        var autoriza = 2;
+        localStorage.setItem("acesso", true);
+        window.location.href = "editarchamado.html";
+    }else{
+        alert("Usuario ou Senha Invalidos!");
+    }
+
+}
 
 $(function(){
 
@@ -67,7 +85,7 @@ $(function(){
 		operacao = "A";
 		return true;
 	}
-	// Função para listar clientes
+	// Função para listar clientes  (cabeçalho)
 	function Listar(){
 		$("#tblListar").html("");
 		$("#tblListar").html(
@@ -92,6 +110,8 @@ $(function(){
 		// Malha de repetição para inserir todos os registros
 		 for(var i in tbClientes){
 			var cli = JSON.parse(tbClientes[i]);
+			// Formatar data para o format brasileiro dia, mes, ano
+			var dtfinal = cli.Data.substring(8,10) + "/" +cli.Data.substring(5,7)  +"/"  +cli.Data.substring(0,4);
 		  	$("#tblListar tbody").append("<tr>"+
 			  							"	<td></td>" + 
 									 	 "	<td>"+cli.Codigo+"</td>" + 
@@ -158,9 +178,68 @@ $(function(){
 		Excluir();
 		Listar();
 	});
+
+	// Código automatico //
+	var ultimo = JSON.parse(tbClientes.slice(-1));
+	var ultconv = parseInt(ultimo.Codigo);
+	
+			$("#txtCodigo").val(ultconv+1);
+
 	// Ação com base nos eventos do número do Código
 	$("#txtcodigo").val (ultconv+1);
 	var ultimo=JSON.parse (tbClientes.slice(-1));
 	var ultimo=parseInt (ultimo.codigo);
+
+
+	// status
+$("#txtNome").change(function () {
+	//alert( $( this ).val() );
+	var pessoa = $(this).val();
+
+	if (pessoa == "Wilson") {
+		$("#txtStatus").val('Em aberto');
+	} else
+		$("#txtStatus").val('Em andamento');
+});
+
+// Obtém a data/hora atual
+	
+	var data = new Date();
+	
+	// Guarda cada pedaço em uma variável
+	var dia     = data.getDate();           // 1-31
+	var dia_sem = data.getDay();            // 0-6 (zero=domingo)
+	var mes     = data.getMonth();          // 0-11 (zero=janeiro)
+	var ano2    = data.getYear();           // 2 dígitos
+	var ano4    = data.getFullYear();       // 4 dígitos
+	var hora    = data.getHours();          // 0-23
+	var min     = data.getMinutes();        // 0-59
+	var seg     = data.getSeconds();        // 0-59
+	var mseg    = data.getMilliseconds();   // 0-999
+	var tz      = data.getTimezoneOffset(); // em minutos
+	
+	if(dia<10) {
+		dia = '0'+dia;
+	} 
+  
+	if(mes<10) {
+		mes = '0'+ (mes+1);
+	} 
+
+	// Formata a data e a hora (note o mês + 1)
+	var str_data = dia + '/' + (mes+1) + '/' + ano4; // Brasil
+	var str_data_Brazil = ano4 + '-' + mes + '-' + dia; // europeu
+	var hora_geral = hora + ':'+ min;
+	//var str_hora = hora + ':' + min + ':' + seg;
+
+	// Mostra o resultado
+	//alert('Hoje é ' + str_data + ' às ' + str_hora);
+	$("#txtData").val(str_data_Brazil);
+	$("#txtHora").val(hora_geral);
+
+	//alert(hora_geral);
+
+	
+	
 	
 });
